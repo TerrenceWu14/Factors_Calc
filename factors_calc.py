a9b39d74dfceefc747a24dfb7b391aaa003ee801
TerrenceWu14 <135181592+TerrenceWu14@users.noreply.github.com>
2023-07-25 14:44:32 +1200

    Add files via upload

---
+++ factors_calc.py
@@ -0,0 +1,32 @@
+# Functions go here
+
+# Puts series of symbols at the start and end of the text (for emphasis)
+def statement_generator(text, decoration):
+    # Make string with five characters
+    ends = decoration * 5
+
+    # Add decoration to start and end of statement
+    statement = "{} {} {}".format(ends, text, ends)
+
+    print()
+    print(statement)
+    print()
+
+    return ""
+
+# Displays instruction / information
+def instructions():
+
+    statement_generator("Instructions / Information", "=")
+    print()
+    print(" Please enter an integer between 1 and 200 (with no decimals)")
+    print()
+    print("This program assumes that images are being represented in 24 bit color (ie: 24 bits per pixel). "
+          "For text we assume that ascii encoding is being used (8 bits per character)")
+    print()
+    print("Complete as many calculations as necessary, "
+          "pressing <enter> at the end of each calculator or any key to quit.")
+    print()
+    return ""
+
+# Main Routine
