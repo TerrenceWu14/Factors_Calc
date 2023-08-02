673518bffaadd351a80ed375d079ca3f3a1b7d4e
TerrenceWu14 <135181592+TerrenceWu14@users.noreply.github.com>
2023-08-02 13:19:21 +1200

    Add files via upload

---
+++ ZZ_modulo_play.py
@@ -0,0 +1,23 @@
+# Modulo example
+
+keep_going = ""
+while keep_going == "":
+
+    num_lollies = int(input("How many lollies? "))
+    num_students = int(input("How many students? "))
+
+    # Lollies per student (divide)
+    lollies_per_student = num_lollies // num_students
+    lollies_left = num_lollies % num_students
+
+    # Output fixer (lolly vs lollies)
+    if lollies_left == 1:
+        lolly_pl = "lolly"
+    else:
+        lolly_pl = "lollies"
+
+    # Output...
+    print()
+    print(" You have {} lollies and {} students"
+          .format(num_lollies, num_students))
+
